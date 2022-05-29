d9d6759a8a2f5e9dfd42c01db49dd721a6adcfd8
umarmuhd <umarfarouqft@gmail.com>
2022-05-29 14:58:07 +0100

    fix jwt

--- server/src/modules/auth/auth.controller.ts
+++ server/src/modules/auth/auth.controller.ts
@@ -41,7 +41,7 @@ export async function loginHandler(req: Request, res: Response) {
   res.cookie('accessToken', jwt, {
     maxAge: 3.154e10, // 1 year
     httpOnly: true,
-    domain: 'checkgate.ml',
+    domain: 'checkgate',
     path: '/',
     sameSite: 'strict',
     secure: false,
